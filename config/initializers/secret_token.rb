f528e832f1c1b2dbc1f51e2b2d42cb02c407559a
Yiyang Song <songyiyang2012@gmail.com>
2014-03-13 15:31:24 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecidVis::Application.config.secret_key_base = '3a51867798fda6b8fe52f02ce9f0cdda3cf94c464ea93cd790d1199e867adc7a576f9f469b5e9e694a4600481e7c6788bd48705a655b58c2e1e0b74c03172b41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecidVis::Application.config.session_store :cookie_store, key: '_recid_vis_session'
